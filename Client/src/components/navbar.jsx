3b35e38185ae837fa9a5450ccdf8364a7b833c8c
anas20023 <yorder07@gmail.com>
2024-03-16 16:26:42 +0600

    Update navbar styling and content

--- Client/src/components/navbar.jsx
+++ Client/src/components/navbar.jsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 const navbar = () => {
     return <>
-        <nav className="flex flex-row justify-between items-center w-4/5  max-w-screen-lg mx-auto py-5">
-            <div className="flex flex-row justify-between items-center w-[70%]">
-                <h1 className="text-2xl font-bold text-orange-700 cursor-pointer">Halal Food</h1>
+        <nav className="flex flex-row justify-between items-center w-4/5  max-w-screen-lg mx-auto py-5 font-[poppins]">
+            <div className="flex flex-row justify-between items-center w-[65%]">
+                <h1 className="text-2xl font-bold text-sky-700 cursor-pointer">BUIPC-2024</h1>
             </div>
-            <div className="flex flex-row justify-around items-center w-[30%] text-lg font-semibold text-slate-900">
+            <div className="flex flex-row justify-around items-center w-[35%] text-lg font-medium text-slate-900">
                 <a href="#">Home</a>
                 <a href="#">About</a>
                 <a href="#">Services</a>
