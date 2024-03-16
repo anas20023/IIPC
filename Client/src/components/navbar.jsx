0a569923c12d03d7ac3a8aa72b2236adebbd679d
anas20023 <yorder07@gmail.com>
2024-03-16 16:29:58 +0600

    Update page title and navbar styling

--- Client/src/components/navbar.jsx
+++ Client/src/components/navbar.jsx
@@ -3,12 +3,11 @@ const navbar = () => {
     return <>
         <nav className="flex flex-row justify-between items-center w-4/5  max-w-screen-lg mx-auto py-5 font-[poppins]">
             <div className="flex flex-row justify-between items-center w-[65%]">
-                <h1 className="text-2xl font-bold text-sky-700 cursor-pointer">BUIPC-2024</h1>
+                <h1 className="text-2xl font-bold text-sky-600 cursor-pointer">BUIPC-2024</h1>
             </div>
-            <div className="flex flex-row justify-around items-center w-[35%] text-lg font-medium text-slate-900">
-                <a href="#">Home</a>
-                <a href="#">About</a>
-                <a href="#">Services</a>
+            <div className="flex flex-row justify-evenly items-end w-[35%] text-lg font-medium text-slate-900">
+                <a href="#">Login</a>
+                <a href="#">Register</a>
                 <a href="#">Contact</a>
             </div>
 
