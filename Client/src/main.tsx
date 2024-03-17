736cf5a8969483d83a16eb1fc1e19f4a6c69e5cc
anas20023 <yorder07@gmail.com>
2024-03-17 23:28:37 +0600

    Remove navbar component

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Navbar from './navbar.tsx'
+import Navbar from './Components/navbar.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
