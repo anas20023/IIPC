cd66f5bb1f253e6664f97364f83d55a7f8a794b3
anas20023 <yorder07@gmail.com>
2024-04-04 15:48:54 +0600

    Update Card and Navbar components

--- Client/src/Components/Card.tsx
+++ Client/src/Components/Card.tsx
@@ -2,7 +2,7 @@ export default function Card(obj) {
     return (
         <div className="flex flex-row justify-evenly lg:justify-center items-center border-2 h-56 my-3 w-[94%] mx-auto rounded-lg shadow-md max-w-screen-lg px-4 gap-8 lg:gap-0">
             <div className="flex flex-col justify-center items-center w-1/2">
-                <img src={obj.img} alt="Random Image" className="w-44 h-44 rounded-full mt-4 ring-4 object-cover ring-green-500" />
+                <img src={obj.img} alt="Random Image" className="w-40 h-40 rounded-full mt-4 ring-4 object-cover ring-green-500" />
             </div>
             <div className="flex flex-col justify-between items-start text-slate-800 w-1/2 lg:-ml-10">
                 <h1 className={'text-2xl font-black ' + obj.color}>{obj.Position}<sup>{obj.sup}</sup> Position</h1>
--- Client/src/Components/navbar.tsx
+++ Client/src/Components/navbar.tsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     <nav className="flex flex-row justify-between items-center max-w-5xl w-9/12 mx-auto py-4">
       <h1 className="text-2xl font-bold text-slate-800">IIPC <span className="text-blue-600">52</span></h1>
       <div className="flex flex-row justify-between items-center">
-        <a href="#" className="text-md lg:text-lg font-medium text-white bg-blue-600 px-2 lg:px-4 py-2 rounded">Management</a>
+        <a href="#" className="text-md lg:text-lg font-medium text-white bg-blue-600 px-2 lg:px-4 py-2 rounded">Winners</a>
       </div>
     </nav>
   )
